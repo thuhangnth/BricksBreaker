f378ea88ced4c30524f6dfd1dccbd33e3ca185db
Thu Hang <boongcd@yahoo.com>
2016-03-18 22:53:45 +0800

    First commit

---
+++ drawScreen.c
@@ -0,0 +1,89 @@
+#include "drawScreen.h"
+
+int initTFT()
+{
+	int Status;
+	XTft_Config *TftConfigPtr;
+
+	/*
+	 * Get address of the XTft_Config structure for the given device id.
+	 */
+	TftConfigPtr = XTft_LookupConfig(TFT_DEVICE_ID);
+	if (TftConfigPtr == (XTft_Config *)NULL) 
+	{
+		return XST_FAILURE;
+	}
+
+	/*
+	 * Initialize all the TftInstance members and fills the screen with
+	 * default background color.
+	 */
+	Status = XTft_CfgInitialize(&TftInstance, TftConfigPtr,
+			TftConfigPtr->BaseAddress);
+	if (Status != XST_SUCCESS) 
+	{
+		return XST_FAILURE;
+	}
+
+	/*
+	 * Wait till Vsync(Video address latch) status bit is set before writing
+	 * the frame address into the Address Register. This ensures that the
+	 * current frame has been displayed and we can display a new frame of
+	 * data. Checking the Vsync state ensures that there is no data flicker
+	 * when displaying frames in real time though there is some delay due to
+	 * polling.
+	 */
+	while (XTft_GetVsyncStatus(&TftInstance) !=
+			XTFT_IESR_VADDRLATCH_STATUS_MASK);
+
+	/*
+	 * Change the Video Memory Base Address from default value to
+	 * a valid Memory Address and clear the screen.
+	 */
+	XTft_SetFrameBaseAddr(&TftInstance, TFT_FRAME_ADDR);
+	XTft_ClearScreen(&TftInstance);
+
+	XTft_SetPos(&TftInstance, 0,0);
+	XTft_SetPosChar(&TftInstance, 0,0);
+
+	XTft_SetColor(&TftInstance, 0x00000000, 0x00ffffff);
+
+	XTft_FillScreen(&TftInstance, 0, 0,639,479,0x00ffffff); // white
+
+	return XST_SUCCESS;
+}
+
+void drawHorLine(int x0, int x1, int y, int col)
+{
+	int i = 0;
+	for (i=x0;i<=x1;i++)
+	{
+		XTft_SetPixel(&TftInstance, i, y, col);
+	}
+}
+
+void drawRect(int x, int y, int w, int l, int col)
+{
+	int i = 0;
+	int x1 = x;
+	int x2 = x+l-1;
+	for (i=y;i<y+w;i++)
+	{
+		drawHorLine(x,x2,i,col);
+	}
+}
+
+void drawBrickCol(int columnIndex, int brickRemain, colBrick, colBG)
+{
+	int x = 64 + (BRICK_LENGTH+SPACE)*colIndex;
+	int i;
+	for (i=0;i<MAX_ROW;i++)
+	{
+		y = 64+(BRICK_WIDHT+SPACE)*i;
+		if (i<brickRemain)
+		{
+			drawRect(x,y,5,45,colBrick);
+		}
+		
+	}
+}
---
+++ drawScreen.h
@@ -0,0 +1,47 @@
+/***************************** Include Files ********************************/
+
+#include "xtft.h"
+#include "xparameters.h"
+#include <string.h>
+
+/************************** Constant Definitions ****************************/
+/**
+ * The following constants map to the XPAR parameters created in the
+ * xparameters.h file. They are defined here such that a user can easily
+ * change all the needed parameters in one place.
+ */
+#define TFT_DEVICE_ID    XPAR_TFT_0_DEVICE_ID
+#define DDR_HIGH_ADDR    XPAR_PS7_DDR_0_S_AXI_HIGHADDR 
+
+#ifndef DDR_HIGH_ADDR
+#warning "CHECK FOR THE VALID DDR ADDRESS IN XPARAMETERS.H"
+#endif
+
+#define DISPLAY_COLUMNS  640
+#define DISPLAY_ROWS     480
+
+
+/**
+ * User has to specify a 2MB memory space for filling the frame data.
+ * This constant has to be updated based on the memory map of the
+ * system.
+ */
+#define TFT_FRAME_ADDR        0x10000000
+
+/**************************** Type Definitions ******************************/
+
+/************************** Function Prototypes *****************************/
+
+int initTFT();
+void drawBrickCol(int columnIndex, int brickRemain, colBrick, colBG); //columnIndex = 0..9
+void drawRect(int x, int y, int w, int l, int col); //(x,y) is the top left corner of the rectangle
+void drawHorLine(int x1, int x2, int y, int col);
+
+
+/************************** Variable Definitions ****************************/
+
+const int MAX_ROW = 8;
+const int BRICK_WIDTH = 5;
+const int BRICK_LENGTH = 40;
+const int SPACE = 5;
+static XTft TftInstance;
