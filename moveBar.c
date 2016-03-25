3c2bf931f871431d59e2745afb136d1b64b075f5
mason91 <oblivion.mason@gmail.com>
2016-03-25 17:50:23 +0800

    Update moveBar.c

--- moveBar.c
+++ moveBar.c
@@ -1,40 +1,33 @@
 #include "moveBar.h"
 
-static void gpPBIntHandler(void *arg) //Should be very short (in time). In a practical program, don't print etc.
-{
-	unsigned char val;
-	//clear the interrupt flag. if this is not done, gpio will keep interrupting the microblaze.--
-	// --Possible to use (XGpio*)arg instead of &gpPB
-	XGpio_InterruptClear(&gpPB,1);
-	long int_time = xget_clock_ticks();
-	val = XGpio_DiscreteRead(&gpPB, 1);
-	if (val>0) {
-		if (int_time - last_int_time1>20 && int_time - last_int_time0> 20)
-		{
-			//Read the state of the push buttons.
-			btn = val;
-		}
-		last_int_time1 = int_time;
-	}
-	else {
-		if (int_time - last_int_time0 > 20)
-		{
-			//Read the state of the push buttons.
-			btn = val;
-		}
-		last_int_time0 = int_time;
-	}
-}
+#define colBG 0x00FFFFFF
+#define colBar 0x00FF0000
+#define LEFT_BTN 4
+#define RIGHT_BTN 8
+#define LENGTH 80
+#define DIST 10
+
+XGpio gpPB; //PB device instance.
+long lastlast_int_time1 = 0, lastlast_int_time0 = 0;
+int btn;
+
+typedef struct{
+	int x;
+	int y;
+	int dir, col;
+}Bar;
+
+static Bar bar;
 
 void moveBar()
 {
 	if (btn==LEFT_BTN)
-	{	
+	{
 		bar.dir = LEFT_BTN;
 		//Decrement x coordinate
 		bar.x = bar.x-DIST;
 		if (bar.x<60)  //Bar hits left wall
-		{		
+		{
 		bar.x=60;
 		}
 	}
@@ -45,10 +38,10 @@ void moveBar()
 		bar.x = bar.x+DIST;
 		if (bar.x>454-LENGTH+1) //Bar hits right wall
 		{
-			bar.x=545-LENGTH+1;
+			bar.x=454-LENGTH+1;
 		}
 	}
-	
+
 }
 
 void drawBar()
@@ -60,14 +53,20 @@ void drawBar()
 	}
 	else if(bar.dir == RIGHT_BTN)
 	{
-		drawRect(bar.x-DIST,bar.y,5,DIST,colBG); 
+		drawRect(bar.x-DIST,bar.y,5,DIST,colBG);
 	}
+}
+
+void initBar() {
+	bar.x = 226;
+	bar.y = 405;
+	bar.col = colBar;
 }
 
 void initInt()
 {
 	// Initialise the PB instance
-	Status = XGpio_Initialize(&gpPB, XPAR_GPIO_0_DEVICE_ID);
+	XGpio_Initialize(&gpPB, XPAR_GPIO_0_DEVICE_ID);
 	// set PB gpio direction to input.
 	XGpio_SetDataDirection(&gpPB, 1, 0x000000FF);
 
@@ -80,5 +79,40 @@ void initInt()
 	register_int_handler(XPAR_MICROBLAZE_0_AXI_INTC_AXI_GPIO_0_IP2INTC_IRPT_INTR, gpPBIntHandler, &gpPB);
 	//enable the interrupt in xilkernel
 	enable_interrupt(XPAR_MICROBLAZE_0_AXI_INTC_AXI_GPIO_0_IP2INTC_IRPT_INTR);
-	
+}
+
+void gpPBIntHandler(void *arg) //Should be very short (in time). In a practical program, don't print etc.
+{
+	unsigned char val;
+	//clear the interrupt flag. if this is not done, gpio will keep interrupting the microblaze.--
+	// --Possible to use (XGpio*)arg instead of &gpPB
+	XGpio_InterruptClear(&gpPB,1);
+	long int_time = xget_clock_ticks();
+	val = XGpio_DiscreteRead(&gpPB, 1);
+	if (val>0) {
+		if (int_time - lastlast_int_time1>10 && int_time - lastlast_int_time0> 10)
+		{
+			//Read the state of the push buttons.
+			btn = val;
+		}
+		lastlast_int_time1 = int_time;
+	}
+	else {
+		if (int_time - lastlast_int_time0 > 10)
+		{
+			//Read the state of the push buttons.
+			btn = val;
+		}
+		lastlast_int_time0 = int_time;
+	}
+}
+
+int getBar_x0()
+{
+	return bar.x;
+}
+
+int getBar_x1()
+{
+	return bar.x+LENGTH-1;
 }
