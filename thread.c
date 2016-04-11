241aaa13782fde144387384bd7a42c40ea398b30
mason91 <oblivion.mason@gmail.com>
2016-04-11 11:31:24 +0800

    Update thread.c

--- thread.c
+++ thread.c
@@ -1,159 +1,403 @@
 #include "xmk.h"
 #include <pthread.h>
 #include <errno.h>
-#include "xmutex.h"
-#include "drawScreen.h"
+#include "semaphore.h"
+#include "xmbox.h"
+#include <xparameters.h>
 
-
-pthread_t tid1,tid2,tid3,tid4,tid5,tid6,tid7,tid8,tid9,tid10;
-
+#define MY_CPU_ID XPAR_CPU_ID
+#define MBOX_DEVICE_ID XPAR_MBOX_0_DEVICE_ID
 #define brickColour 0x00a52a2a
+#define GOLD 0x00FFD700
 #define bgColour 0x0000b200
+#define radius 7
 
-#define MUTEX_DEVICE_ID XPAR_MUTEX_0_IF_1_DEVICE_ID
-#define MUTEX_NUM 0
-XMutex Mutex;
+pthread_t tid1,tid2,tid3,tid4,tid5,tid6,tid7,tid8,tid9,tid10,tid11;
+
+typedef struct
+{
+	int id;
+	int draw[8];
+	int col;
+}Brick;
+
+typedef struct {
+	int dir_x,dir_y,colour,x,y,prevX,prevY,nextX,nextY;
+}ball;
+
+
+XMbox Mbox;
+sem_t sem, updateBall;
+static ball fishball;
+volatile int thread = -1;
+
+static void Mailbox_Receive(XMbox *MboxInstancePtr, ball *ball_pointer)
+{
+	XMbox_ReadBlocking(MboxInstancePtr,	ball_pointer,36);
+}
+
+void changeBall(int whichSide, int dirX, int dirY, int dirArray[]) {
+	//top
+	if(whichSide == 0) {
+		dirArray[0] = dirX;
+		dirArray[1] = dirY * -1;
+	}
+	//btm
+	else if(whichSide == 1) {
+		dirArray[0] = dirX;
+		dirArray[1] = dirY * -1;
+	}
+	//left
+	else if(whichSide == 2) {
+		dirArray[0] = dirX * -1;
+		dirArray[1] = dirY;
+	}
+	//right
+	else {
+		dirArray[0] = dirX * -1;
+		dirArray[1] = dirY;
+	}
+}
+
+void detectCollisionColumn(int column, int y, int x, int dirX, int dirY, int dirArray[], int draw[]) {
+	int index = -1;
+	int leftBound = column*65;
+	int rightBound = leftBound+39;
+	index = (y-65)/20;
+	if (index<8) {
+		//collision from top/btm
+		if(x-radius<=rightBound || x+radius>=leftBound) {
+			//coming from top
+			if (dirY == 1) {
+				index += 1;
+				if (draw[index]) {
+					if(y+radius >= index*20+65) {
+						changeBall(0,dirX,dirY,dirArray);
+						draw[index] = 0;
+					}
+				}
+			}
+			//coming from btm
+			else {
+				if(draw[index]) {
+					if(y-radius <= index*20+79) {
+						changeBall(1,dirX,dirY,dirArray);
+						draw[index] = 0;
+					}
+				}
+			}
+		}
+		//collision from side
+		else {
+			//coming from left
+			if(dirX == 1) {
+				if(x+radius >= leftBound) {
+					//topBrick
+					if(y-radius <= index*20+79) {
+						if(draw[index]) {
+							changeBall(2,dirX,dirY,dirArray);
+							draw[index] = 0;
+						}
+					}
+					//bottomBrick
+					else if(y+radius >= index*20+65) {
+						index += 1;
+						if(draw[index]) {
+							changeBall(2,dirX,dirY,dirArray);
+							draw[index] = 0;
+						}
+					}
+				}
+			}
+			else {
+				//coming from right
+				if(x-radius <= rightBound) {
+					//topBrick
+					if(y-radius <= index*20+79) {
+						if(draw[index]) {
+							changeBall(3,dirX,dirY,dirArray);
+							draw[index] = 0;
+						}
+					}
+					//bottomBrick
+					else if(y+radius >= index*20+65) {
+						index += 1;
+						if(draw[index]) {
+							changeBall(3,dirX,dirY,dirArray);
+							draw[index] = 0;
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+int detectCollisionThread(int x, int dirX) {
+	int index = -1;
+	index = (x-65)/45;
+	if(dirX == 1) {
+		index += 1;
+	}
+	return index;
+
+}
+
+void* signalThread() {
+	sem_wait(&updateBall);
+	Mailbox_Receive(&Mbox, &fishball);
+	thread = detectCollisionThread(fishball.nextX, fishball.dir_x);
+	sem_post(&updateBall);
+
+}
+
 
 
 void* brickCol_1 () {
-	int i;
-	XMutex_Lock(&Mutex, MUTEX_NUM);
-	drawBrickCol(1, 8, brickColour, bgColour);
-	XMutex_Unlock(&Mutex, MUTEX_NUM);
+	Brick brick1;
+	brick1.id = 1;
 	while(1)
 	{
-		for(i=0; i<20; i++)
-		{
-			;
+		sem_wait(&sem);
+		//change To Gold
+		brick1.col = GOLD;
+		//Send Mail
+		if (!XMbox_IsFull(&Mbox)) {
+			XMbox_WriteBlocking(&Mbox, &brick1, 12);
+		}
+		sleep(1000);
+
+		sem_post(&sem);
+		//change Back
+		brick1.col = brickColour;
+		//Send Mail
+		if (!XMbox_IsFull(&Mbox)) {
+			XMbox_WriteBlocking(&Mbox, &brick1, 12);
 		}
 	}
 }
 
 void* brickCol_2 () {
-	int i;
-	XMutex_Lock(&Mutex, MUTEX_NUM);
-	drawBrickCol(2, 8, brickColour, bgColour);
-	XMutex_Unlock(&Mutex, MUTEX_NUM);
+	Brick brick2;
+	brick2.id = 2;
 	while(1)
 	{
-		for(i=0; i<20; i++)
-				{
-					;
-				}
+
+		sem_wait(&sem);
+		//change To Gold
+		brick2.col = GOLD;
+		//Send Mail
+		if (!XMbox_IsFull(&Mbox)) {
+			XMbox_WriteBlocking(&Mbox, &brick2, 12);
+		}
+		sleep(789);
+
+		sem_post(&sem);
+		//change Back
+		brick2.col = brickColour;
+		//Send Mail
+		if (!XMbox_IsFull(&Mbox)) {
+			XMbox_WriteBlocking(&Mbox, &brick2, 12);
+		}
 	}
 }
 
 void* brickCol_3 () {
-	int i;
-	XMutex_Lock(&Mutex, MUTEX_NUM);
-	drawBrickCol(3, 8, brickColour, bgColour);
-	XMutex_Unlock(&Mutex, MUTEX_NUM);
+	Brick brick3;
+	brick3.id = 3;
 	while(1)
 	{
-		for(i=0; i<20; i++)
-				{
-					;
-				}
+
+		sem_wait(&sem);
+		//change To Gold
+		brick3.col = GOLD;
+		//Send Mail
+		if (!XMbox_IsFull(&Mbox)) {
+			XMbox_WriteBlocking(&Mbox, &brick3, 12);
+		}
+		sleep(345);
+		sem_post(&sem);
+
+		//change Back
+		brick3.col = brickColour;
+		//Send Mail
+		if (!XMbox_IsFull(&Mbox)) {
+			XMbox_WriteBlocking(&Mbox, &brick3, 12);
+		}
 	}
 }
 
 void* brickCol_4 () {
-	int i;
-	XMutex_Lock(&Mutex, MUTEX_NUM);
-	drawBrickCol(4, 8, brickColour, bgColour);
-	XMutex_Unlock(&Mutex, MUTEX_NUM);
+	Brick brick4;
+	brick4.id = 4;
 	while(1)
 	{
-		for(i=0; i<20; i++)
-				{
-					;
-				}
+
+		sem_wait(&sem);
+		//change To Gold
+		brick4.col = GOLD;
+		//Send Mail
+		if (!XMbox_IsFull(&Mbox)) {
+			XMbox_WriteBlocking(&Mbox, &brick4, 12);
+		}
+		sleep(479);
+		sem_post(&sem);
+
+		//change Back
+		brick4.col = brickColour;
+		//Send Mail
+		if (!XMbox_IsFull(&Mbox)) {
+			XMbox_WriteBlocking(&Mbox, &brick4, 12);
+		}
 	}
 }
 
 void* brickCol_5 () {
-	int i;
-	XMutex_Lock(&Mutex, MUTEX_NUM);
-	drawBrickCol(5, 8, brickColour, bgColour);
-	XMutex_Unlock(&Mutex, MUTEX_NUM);
+	Brick brick5;
+	brick5.id = 5;
 	while(1)
 	{
-		for(i=0; i<20; i++)
-				{
-					;
-				}
+
+		sem_wait(&sem);
+		//change To Gold
+		brick5.col = GOLD;
+		//Send Mail
+		if (!XMbox_IsFull(&Mbox)) {
+			XMbox_WriteBlocking(&Mbox, &brick5, 12);
+		}
+		sleep(777);
+		sem_post(&sem);
+
+		//change Back
+		brick5.col = brickColour;
+		//Send Mail
+		if (!XMbox_IsFull(&Mbox)) {
+			XMbox_WriteBlocking(&Mbox, &brick5, 12);
+		}
 	}
 }
 
 void* brickCol_6 () {
-	int i;
-	XMutex_Lock(&Mutex, MUTEX_NUM);
-	drawBrickCol(6, 8, brickColour, bgColour);
-	XMutex_Unlock(&Mutex, MUTEX_NUM);
+	Brick brick6;
+	brick6.id = 6;
 	while(1)
 	{
-		for(i=0; i<20; i++)
-				{
-					;
-				}
+
+		sem_wait(&sem);
+		//change To Gold
+		brick6.col = GOLD;
+		//Send Mail
+		if (!XMbox_IsFull(&Mbox)) {
+			XMbox_WriteBlocking(&Mbox, &brick6, 12);
+		}
+		sleep(890);
+		sem_post(&sem);
+
+		//change Back
+		brick6.col = brickColour;
+		//Send Mail
+		if (!XMbox_IsFull(&Mbox)) {
+			XMbox_WriteBlocking(&Mbox, &brick6, 12);
+		}
 	}
 }
 
 void* brickCol_7 () {
-	int i;
-	XMutex_Lock(&Mutex, MUTEX_NUM);
-	drawBrickCol(7, 8, brickColour, bgColour);
-	XMutex_Unlock(&Mutex, MUTEX_NUM);
+	Brick brick7;
+	brick7.id = 7;
 	while(1)
 	{
-		for(i=0; i<20; i++)
-				{
-					;
-				}
+
+		sem_wait(&sem);
+		//change To Gold
+		brick7.col = GOLD;
+		//Send Mail
+		if (!XMbox_IsFull(&Mbox)) {
+			XMbox_WriteBlocking(&Mbox, &brick7, 12);
+		}
+		sleep(1123);
+		sem_post(&sem);
+
+		//change Back
+		brick7.col = brickColour;
+		//Send Mail
+		if (!XMbox_IsFull(&Mbox)) {
+			XMbox_WriteBlocking(&Mbox, &brick7, 12);
+		}
 	}
 }
 
 void* brickCol_8 () {
-	int i;
-	XMutex_Lock(&Mutex, MUTEX_NUM);
-	drawBrickCol(8, 8, brickColour, bgColour);
-	XMutex_Unlock(&Mutex, MUTEX_NUM);
+	Brick brick8;
+	brick8.id = 8;
 	while(1)
 	{
-		for(i=0; i<20; i++)
-				{
-					;
-				}
+
+		sem_wait(&sem);
+		//change To Gold
+		brick8.col = GOLD;
+		//Send Mail
+		if (!XMbox_IsFull(&Mbox)) {
+			XMbox_WriteBlocking(&Mbox, &brick8, 12);
+		}
+		sleep(608);
+		sem_post(&sem);
+
+		//change Back
+		brick8.col = brickColour;
+		//Send Mail
+		if (!XMbox_IsFull(&Mbox)) {
+			XMbox_WriteBlocking(&Mbox, &brick8, 12);
+		}
 	}
 }
 
 void* brickCol_9 () {
-	int i;
-	XMutex_Lock(&Mutex, MUTEX_NUM);
-	drawBrickCol(9, 8, brickColour, bgColour);
-	XMutex_Unlock(&Mutex, MUTEX_NUM);
+	Brick brick9;
+	brick9.id = 9;
 	while(1)
 	{
-		for(i=0; i<20; i++)
-				{
-					;
-				}
+
+		sem_wait(&sem);
+		//change To Gold
+		brick9.col = GOLD;
+		//Send Mail
+		if (!XMbox_IsFull(&Mbox)) {
+			XMbox_WriteBlocking(&Mbox, &brick9, 12);
+		}
+		sleep(945);
+		sem_post(&sem);
+
+		//change Back
+		brick9.col = brickColour;
+		//Send Mail
+		XMbox_WriteBlocking(&Mbox, &brick9, 12);
 	}
 }
 
 void* brickCol_10 () {
-	int i;
-	XMutex_Lock(&Mutex, MUTEX_NUM);
-	drawBrickCol(10, 8, brickColour, bgColour);
-	XMutex_Unlock(&Mutex, MUTEX_NUM);
+	Brick brick10;
+	brick10.id = 10;
 	while(1)
 	{
-		for(i=0; i<20; i++)
-				{
-					;
-				}
+
+		sem_wait(&sem);
+		//change To Gold
+		brick10.col = GOLD;
+		//Send Mail
+		if (!XMbox_IsFull(&Mbox)) {
+			XMbox_WriteBlocking(&Mbox, &brick10, 12);
+		}
+		sleep(1234);
+		sem_post(&sem);
+
+		//change Back
+		brick10.col = brickColour;
+		//Send Mail
+		XMbox_WriteBlocking(&Mbox, &brick10, 12);
 	}
 }
+
 
 int main_prog(void);
 int main (void) {
@@ -170,19 +414,40 @@ int main (void) {
 
 int main_prog(void) {
 	int ret;
-	XStatus Status;
-
-	XMutex_Config*ConfigPtr;
-	ConfigPtr = XMutex_LookupConfig(MUTEX_DEVICE_ID);
-	if (ConfigPtr == (XMutex_Config *)NULL){
-
-		xil_printf("B1-- ERROR  init HW mutex...\r\n");
+	int Status;
+	XMbox_Config *ConfigPtr;
+	ConfigPtr = XMbox_LookupConfig(MBOX_DEVICE_ID);
+	if (ConfigPtr == (XMbox_Config*)NULL)
+	{
+		print("--Error configuring Mbox Sender--\r\n");
+		return XST_FAILURE;
 	}
 
-	Status = XMutex_CfgInitialize(&Mutex, ConfigPtr, ConfigPtr->BaseAddress);
+	Status = XMbox_CfgInitialize(&Mbox, ConfigPtr, ConfigPtr->BaseAddress);
+	if (Status != XST_SUCCESS)
+	{
+		print("--Error initializing Mbox Sender--\r\n");
+		return XST_FAILURE;
+	}
 
-	initTFT();
-	startScreen(bgColour, brickColour, brickColour);
+	if(sem_init(&sem, 1, 2) <0)
+	{
+		print("Error while initializing semaphore sem. \r\n");
+	}
+
+	if(sem_init(&updateBall, 1, 1 <0))
+	{
+		print("Error while initializing semaphore updateBall. \r\n");
+	}
+
+	ret = pthread_create (&tid11, NULL, (void*)signalThread, NULL);
+	if (ret != 0) {
+		xil_printf ("-- ERROR (%d) launching signalThread...\r\n", ret);
+	}
+	else {
+		xil_printf ("Thread signalThread launched with ID %d \r\n", tid11);
+	}
+
 	ret = pthread_create (&tid1, NULL, (void*)brickCol_1, NULL);
 	if (ret != 0) {
 		xil_printf ("-- ERROR (%d) launching brickCol_1...\r\n", ret);
